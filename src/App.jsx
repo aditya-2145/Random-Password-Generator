1daba53b462e70374307672b8ebe8d8206061669
Aditya Gupta <adityakasyaph@gmail.com>
2025-12-03 23:52:17 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ function App() {
     (symbol ? 1 : 0);
 
   return (
-    <div className="container">
+    <div>
       <div className="completePage">
         <div className="box">
           <h3>PASSWORD GENERATOR</h3>
